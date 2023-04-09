e9f395de7e4a41948d47040bc5ffb442bf02365e
Victoria Mertens <victoria.mertens44@gmail.com>
2023-04-09 13:37:07 -0500

    Connected server to client via HTTP request, config for proxy setup

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -2,9 +2,11 @@ import express from 'express';
 
 const app = express();
 
-app.use(express.static('build'));
-
 const PORT = process.env.PORT || 5000;
+
+app.get('/api/test', (req, res) => {
+  res.send('Server Connected');
+});
 
 app.listen(PORT, () => {
   console.log('Listening on port:', PORT);
