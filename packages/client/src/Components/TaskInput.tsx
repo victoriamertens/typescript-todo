20dbb8a8f714a062547e609a4710d46c10279ecf
Victoria Mertens <victoria.mertens44@gmail.com>
2023-04-14 12:49:56 -0500

    Added inputs, and state management for inputs

--- packages/client/src/Components/TaskInput.tsx
+++ packages/client/src/Components/TaskInput.tsx
@@ -1,12 +1,53 @@
-import { typedGet, typedPost, GetResponse, Entry } from '../api/promise';
+import { typedPost } from '../api/promise';
+import { useState } from 'react';
 
 export default function TaskInput() {
-  const name: string = 'hardcodingname';
-  const description: string = 'hardcodeddescription';
+  const [name, setName] = useState<string | undefined>(undefined);
+  const [description, setDescription] = useState<string | undefined>(undefined);
+
+  function handleNameChange(event: React.ChangeEvent<HTMLInputElement>) {
+    let value: string = event.target.value;
+    setName(value);
+  }
+
+  function handleDescriptionChange(event: React.ChangeEvent<HTMLInputElement>) {
+    let value: string = event.target.value;
+    setDescription(value);
+  }
+
+  function handleError() {
+    alert('Need to have entries in the inputs');
+  }
 
   return (
-    <button onClick={() => typedPost(name, description)}>
-      Click me to Post
-    </button>
+    <div>
+      <label htmlFor="name">Name</label>
+      <input
+        type="text"
+        id="name"
+        onChange={(event) => {
+          handleNameChange(event);
+        }}
+      ></input>
+      <label htmlFor="description">Description</label>
+      <input
+        type="text"
+        id="description"
+        onChange={(event) => handleDescriptionChange(event)}
+      ></input>
+
+      <button
+        onClick={() => {
+          if (name === undefined || description === undefined) {
+            console.log(name, description);
+            handleError();
+          } else {
+            typedPost(name, description);
+          }
+        }}
+      >
+        Click me to Post
+      </button>
+    </div>
   );
 }
