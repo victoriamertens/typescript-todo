139ab5ee3c53f752c7e4475fbccc220c5049e859
Victoria Mertens <victoria.mertens44@gmail.com>
2023-04-24 11:08:38 -0500

    Get all categories hooked up and working

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,17 +1,28 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import { typedGet, typedPost, GetResponse, Entry } from './api/promise';
+import { typedGet, typedGetCategories, Entry } from './api/promise';
 import { Task } from './Components/Task';
 import TaskInput from './Components/TaskInput';
 
 function App() {
   const [allTasks, setAllTasks] = useState<Entry[] | undefined>(undefined);
+  const [allCategories, setAllCategories] = useState<Entry[] | undefined>(
+    undefined
+  );
 
   useEffect(() => {
     typedGet()
       .then((response) => {
-        console.log('HERE:', response);
+        console.log('Tasks response:', response);
         setAllTasks(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    typedGetCategories()
+      .then((response) => {
+        console.log('Categoires Response:', response);
+        setAllCategories(response.data);
       })
       .catch((error) => {
         console.log(error);
