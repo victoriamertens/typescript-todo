292dbb1d73c14109b0587d357ff8c6ceedac7d9c
Victoria Mertens <victoria.mertens44@gmail.com>
2023-04-24 09:52:52 -0500

    Cleared JSX errors in CSS of SVG, added source for SVG.

--- packages/client/src/assets/DropDownSVG.tsx
+++ packages/client/src/assets/DropDownSVG.tsx
@@ -7,26 +7,18 @@ export function DropDownSVG() {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <rect width="20" height="20" fill="white" fill-opacity="0.01" />
+      <rect width="20" height="20" fill="white" fillOpacity="0.01" />
       <path
         d="M40 28L24 40L8 28"
         stroke="#333"
-        stroke-width="1"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
-      <path
-        d="M8 10H40"
-        stroke="#333"
-        stroke-width="1"
-        stroke-linecap="round"
-      />
-      <path
-        d="M8 18H40"
-        stroke="#333"
-        stroke-width="1"
-        stroke-linecap="round"
-      />
+      <path d="M8 10H40" stroke="#333" strokeWidth="1" strokeLinecap="round" />
+      <path d="M8 18H40" stroke="#333" strokeWidth="1" strokeLinecap="round" />
     </svg>
   );
 }
+
+//SVG Configuration and HTML code from: https://fontawesomeicons.com/svg/icons/arrow-drop-down-fill
