40e16c3099c90e314e6838f965396596aae56906
Victoria Mertens <victoria.mertens44@gmail.com>
2023-04-25 11:08:16 -0500

    Category filter working properly on tasks

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -8,8 +8,9 @@ import { CategorySelector } from './Components/CategorySelector';
 function App() {
   const [allTasks, setAllTasks] = useState<Entry[]>([]);
   const [allCategories, setAllCategories] = useState<Category[]>([]);
-  const [Filter, setFilter] = useState(0);
-  console.log(Filter);
+  const [filter, setFilter] = useState(0);
+  console.log(filter);
+
   useEffect(() => {
     typedGet()
       .then((response) => {
@@ -36,19 +37,35 @@ function App() {
         <TaskInput />
 
         <CategorySelector categories={allCategories} setFilter={setFilter} />
-        {allTasks.map((spot: Entry) => {
-          return (
-            <Task
-              key={spot.id}
-              name={spot.name}
-              description={spot.description}
-              completed={spot.completed}
-              sub_tasks={spot.sub_tasks}
-              id={spot.id}
-              category_id={spot.category_id}
-            />
-          );
-        })}
+        {filter === 0
+          ? allTasks.map((spot: Entry) => {
+              return (
+                <Task
+                  key={spot.id}
+                  name={spot.name}
+                  description={spot.description}
+                  completed={spot.completed}
+                  sub_tasks={spot.sub_tasks}
+                  id={spot.id}
+                  category_id={spot.category_id}
+                />
+              );
+            })
+          : allTasks.map((spot: Entry) => {
+              if (spot.category_id === filter) {
+                return (
+                  <Task
+                    key={spot.id}
+                    name={spot.name}
+                    description={spot.description}
+                    completed={spot.completed}
+                    sub_tasks={spot.sub_tasks}
+                    id={spot.id}
+                    category_id={spot.category_id}
+                  />
+                );
+              }
+            })}
       </div>
     );
   }
