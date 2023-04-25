76e410ee8234ac90ced0cdf83cfdd2eb092617b9
Victoria Mertens <victoria.mertens44@gmail.com>
2023-04-25 10:41:06 -0500

    HTML select tag rendering with all categories properly.

--- packages/client/src/Components/CategorySelector.tsx
+++ packages/client/src/Components/CategorySelector.tsx
@@ -8,5 +8,19 @@ type CategoryProp = {
 export const CategorySelector: FunctionComponent<CategoryProp> = ({
   categories,
 }) => {
-  return <p>Testing category selector {JSON.stringify(categories)}</p>;
+  return (
+    <div>
+      <label htmlFor="category">Filter by Category:</label>
+      <select name="category" id="category">
+        <option value="0">All Tasks</option>
+        {categories.map((category) => {
+          return (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
 };
